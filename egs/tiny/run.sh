7438370b2ab241706ebff454377b9bbdbc224138
Wang Jian <wantee.wang@gmail.com>
2015-06-09 21:07:59 +0800

    add rnn minibatch case

--- egs/tiny/run.sh
+++ egs/tiny/run.sh
@@ -4,6 +4,9 @@ train_file=./data/train
 valid_file=./data/valid
 test_file=./data/test
 
+conf_dir=./conf/
+exp_dir=./exp/
+
 tr_thr=1
 test_thr=1
 
@@ -16,6 +19,8 @@ steps_len=${#stepnames[*]}
 
 . ../steps/path.sh || exit 1
 
+. ../utils/parse_options.sh || exit 1
+
 if [ $# -gt 1 ] || ! shu-valid-range $1 || [ "$1" == "--help" ]; then 
   echo "usage: $0 [steps]"
   echo "e.g.: $0 -3,5,7-9,10-"
@@ -26,6 +31,10 @@ if [ $# -gt 1 ] || ! shu-valid-range $1 || [ "$1" == "--help" ]; then
   echo "   step$st:	${stepnames[$st]}"
   ((st++))
   done
+  echo ""
+  echo "options: "
+  echo "     --conf-dir <conf-dir>         # config directory."
+  echo "     --exp-dir <exp-dir>           # exp directory."
   exit 1
 fi
 
@@ -35,25 +44,25 @@ st=1
 if shu-in-range $st $steps; then
 echo
 echo "Step $st: ${stepnames[$st]} ..."
-../steps/learn_vocab.sh $train_file $valid_file exp || exit 1;
+../steps/learn_vocab.sh $train_file $valid_file $exp_dir || exit 1;
 fi
 ((st++))
 
 if shu-in-range $st $steps; then
 echo
 echo "Step $st: ${stepnames[$st]} ..."
-conf_dir=./conf/maxent
-dir=exp/maxent
-../steps/init_model.sh --config-file $conf_dir/init.conf\
-        exp/vocab.clm $dir || exit 1;
+conf=$conf_dir/maxent
+dir=$exp_dir/maxent
+../steps/init_model.sh --config-file $conf/init.conf\
+        $exp_dir/vocab.clm $dir || exit 1;
 
-../steps/train_model.sh --train-config $conf_dir/train.conf \
-        --test-config ./conf/test.conf \
+../steps/train_model.sh --train-config $conf/train.conf \
+        --test-config $conf_dir/test.conf \
         --train-threads $tr_thr \
         --test-threads $test_thr \
         $train_file $valid_file $dir || exit 1;
 
-../steps/test_model.sh --config-file ./conf/test.conf \
+../steps/test_model.sh --config-file $conf_dir/test.conf \
         --test-threads $test_thr \
         $dir $test_file || exit 1;
 fi
@@ -62,18 +71,18 @@ fi
 if shu-in-range $st $steps; then
 echo
 echo "Step $st: ${stepnames[$st]} ..."
-conf_dir=./conf/rnn
-dir=exp/rnn
-../steps/init_model.sh --config-file $conf_dir/init.conf\
-        exp/vocab.clm $dir || exit 1;
+conf=$conf_dir/rnn
+dir=$exp_dir/rnn
+../steps/init_model.sh --config-file $conf/init.conf\
+        $exp_dir/vocab.clm $dir || exit 1;
 
-../steps/train_model.sh --train-config $conf_dir/train.conf \
-        --test-config ./conf/test.conf \
+../steps/train_model.sh --train-config $conf/train.conf \
+        --test-config $conf_dir/test.conf \
         --train-threads $tr_thr \
         --test-threads $test_thr \
         $train_file $valid_file $dir || exit 1;
 
-../steps/test_model.sh --config-file ./conf/test.conf \
+../steps/test_model.sh --config-file $conf_dir/test.conf \
         --test-threads $test_thr \
         $dir $test_file || exit 1;
 fi
@@ -82,18 +91,18 @@ fi
 if shu-in-range $st $steps; then
 echo
 echo "Step $st: ${stepnames[$st]} ..."
-conf_dir=./conf/rnn+maxent
-dir=exp/rnn+maxent
-../steps/init_model.sh --config-file $conf_dir/init.conf\
-        exp/vocab.clm $dir || exit 1;
+conf=$conf_dir/rnn+maxent
+dir=$exp_dir/rnn+maxent
+../steps/init_model.sh --config-file $conf/init.conf\
+        $exp_dir/vocab.clm $dir || exit 1;
 
-../steps/train_model.sh --train-config $conf_dir/train.conf \
-        --test-config ./conf/test.conf \
+../steps/train_model.sh --train-config $conf/train.conf \
+        --test-config $conf_dir/test.conf \
         --train-threads $tr_thr \
         --test-threads $test_thr \
         $train_file $valid_file $dir || exit 1;
 
-../steps/test_model.sh --config-file ./conf/test.conf \
+../steps/test_model.sh --config-file $conf_dir/test.conf \
         --test-threads $test_thr \
         $dir $test_file || exit 1;
 fi
