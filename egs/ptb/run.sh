d19132857e86ffdc3051f6aa0d2262df0fc3d650
Wang Jian <wantee.wang@gmail.com>
2015-08-13 11:25:15 +0800

    fix bug in run script

--- egs/ptb/run.sh
+++ egs/ptb/run.sh
@@ -76,7 +76,7 @@ fi
 if shu-in-range $st $steps; then
 echo
 echo "Step $st: ${stepnames[$st]} ..."
-../steps/learn_vocab.sh $train_file exp || exit 1;
+../steps/learn_vocab.sh $train_file $exp_dir || exit 1;
 fi
 ((st++))
 
