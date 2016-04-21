6f4a430d479ab92597f7cb3134368366114b6be4
Wang Jian <wantee.wang@gmail.com>
2016-04-21 16:30:53 +0800

    add mr_* to selection

---
+++ egs/selection/local/mr_select.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Begin configuration section.
+# end configuration sections
+
+echo "$0 $@"  # Print the command line for logging
+function print_help()
+{
+  echo "usage: $0 <hdfs_score> <thresh> <exp-dir> <out-selected>"
+  echo "e.g.: $0 hdfs:///tmp/score 0.0 exp hdfs:///tmp/selected"
+  echo "options: "
+}
+
+help_message=`print_help`
+
+. `dirname $0`/../../steps/path.sh || exit 1
+[ -f `dirname $0`/../path.sh ] && . `dirname $0`/../path.sh
+
+. ../utils/parse_options.sh || exit 1
+
+if [ $# -ne 4 ]; then
+  print_help 1>&2
+  exit 1;
+fi
+
+score_dir=$1
+thresh=$2
+dir=$3
+out=$4
+
+log_dir="$dir/log/"
+mkdir -p "$log_dir"
+
+begin_date=`date +"%Y-%m-%d %H:%M:%S"`
+begin_ts=`date +%s`
+
+HADOOP_HOME="$(dirname `which hadoop`)/.."
+hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-streaming*.jar \
+           -D mapred.job.name="Select Text [$thresh] ($1 -> $2)" \
+           -D mapred.reduce.tasks=0 \
+           -input "$score_dir" \
+           -output "$out" \
+           -mapper "perl select.pl $thresh" \
+           -file "$(cd `dirname $0`; pwd)/select.pl" \
+           > "$log_dir/mr_select.log" 2>&1 || exit 1
+
+end_ts=`date +%s`
+echo "$0: Elapse time: $(shu-diff-timestamp $begin_ts $end_ts)"
+
+exit 0;
--- egs/selection/local/prep_data.sh
+++ egs/selection/local/prep_data.sh
@@ -26,8 +26,8 @@ help_message=`print_help`
 
 . ../utils/parse_options.sh || exit 1
 
-if [ $# -ne 1 ]; then 
-  print_help 1>&2 
+if [ $# -ne 1 ]; then
+  print_help 1>&2
   exit 1;
 fi
 
@@ -83,4 +83,3 @@ end_ts=`date +%s`
 echo "$0: Elapse time: $(shu-diff-timestamp $begin_ts $end_ts)"
 
 exit 0;
-
