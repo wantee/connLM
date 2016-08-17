ab4d76566a2b62f8b481e317d04e924c464c146c
Wang Jian <wantee.wang@gmail.com>
2016-08-17 23:49:44 +0800

    fix forward order for recur glues

--- src/graph.c
+++ src/graph.c
@@ -25,7 +25,6 @@
 #include <string.h>
 
 #include <stutils/st_log.h>
-#include <stutils/st_stack.h>
 
 #include "input.h"
 #include "graph.h"
@@ -117,69 +116,220 @@ static int node_add_link(node_t *node, int lk)
     return node->num_link++;
 }
 
-static int graph_dfs(graph_t *graph, int start,
-        st_stack_t *node_stack, bool *on_stack,
-        bool *visited, bool *passed,
-        int *post_order, int *post_i)
+typedef struct _dfs_args_t_ {
+    bool *on_stack;
+    bool *visited;
+    bool *passed;
+    int post_i;
+    int *recur_to; /* store the dest nodes for a recur node. */
+} dfs_args_t;
+
+static void dfs_args_destroy(dfs_args_t *args)
+{
+    if (args == NULL) {
+        return;
+    }
+
+    safe_free(args->on_stack);
+    safe_free(args->visited);
+    safe_free(args->passed);
+    safe_free(args->recur_to);
+    args->post_i = 0;
+}
+
+static int dfs_args_init(dfs_args_t *args, graph_t *graph)
+{
+    size_t i, sz;
+
+    ST_CHECK_PARAM(args == NULL || graph == NULL, -1);
+
+    args->on_stack = (bool *)malloc(sizeof(bool) * graph->num_node);
+    if (args->on_stack == NULL) {
+        ST_WARNING("Failed to malloc on_stack");
+        goto ERR;
+    }
+    memset(args->on_stack, 0, sizeof(bool) * graph->num_node);
+
+    args->visited = (bool *)malloc(sizeof(bool) * graph->num_node);
+    if (args->visited == NULL) {
+        ST_WARNING("Failed to malloc visited");
+        goto ERR;
+    }
+    memset(args->visited, 0, sizeof(bool) * graph->num_node);
+
+    args->passed = (bool *)malloc(sizeof(bool) * graph->num_link);
+    if (args->passed == NULL) {
+        ST_WARNING("Failed to malloc passed");
+        goto ERR;
+    }
+    memset(args->passed, 0, sizeof(bool) * graph->num_link);
+
+    // the num_node should not be a large number
+    sz = graph->num_node * graph->num_node;
+    args->recur_to = (int *)malloc(sizeof(int) * sz);
+    if (args->recur_to == NULL) {
+        ST_WARNING("Failed to malloc recur_to");
+        goto ERR;
+    }
+    for (i = 0; i < sz; i++) {
+        args->recur_to[i] = -1;
+    }
+
+    args->post_i = 0;
+
+    return 0;
+ERR:
+    dfs_args_destroy(args);
+    return -1;
+}
+
+static int graph_dfs(graph_t *graph, int start, int *post_order,
+        dfs_args_t *args)
 {
     node_t *node;
 
-    int l, lk, recur_lk = -1;
-    int to;
+    int l, lk;
+    int to, recur_i;
 
-    void *tmp;
+    ST_CHECK_PARAM(graph == NULL || post_order == NULL || args == NULL, -1);
 
-    ST_CHECK_PARAM(graph == NULL, -1);
-
-    if (st_stack_push(node_stack, (void *)(long)start) != ST_STACK_OK) {
-        ST_WARNING("Failed to st_stack_push node[%d].", start);
-        return -1;
-    }
-#ifdef _GRAPH_DEBUG_
-    ST_DEBUG("Node push: %d", start);
-#endif
-    visited[start] = true;
-    on_stack[start] = true;
+    args->visited[start] = true;
+    args->on_stack[start] = true;
 
 #ifdef _GRAPH_DEBUG_
     ST_DEBUG("DFS: %d", start);
 #endif
 
+    recur_i = 0;
     node = graph->nodes + start;
     for (l = 0; l < node->num_link; l++) {
         lk = node->links[l];
-        passed[lk] = true;
+        args->passed[lk] = true;
 
         to = graph->links[lk].to;
-        if(!visited[to]) {
-            if (graph_dfs(graph, to, node_stack, on_stack,
-                        visited, passed, post_order, post_i) < 0) {
+        if(!args->visited[to]) {
+            if (graph_dfs(graph, to, post_order, args) < 0) {
                 ST_WARNING("Failed to graph_dfs[%d].", to);
                 return -1;
             }
-        } else if(on_stack[to]) {
+        } else if(args->on_stack[to]) {
             graph->glues[start]->recur = true;
-            /* FIXME: we assume that there will not be multiple recur
-               links for one node. */
-            ST_WARNING("%d, %d", start, to);
-            recur_lk = lk;
+            if (to != start) {
+                args->recur_to[start*graph->num_node + recur_i] = to;
+                recur_i++;
+#ifdef _GRAPH_DEBUG_
+                ST_DEBUG("recur_to[%d]", to);
+#endif
+            }
         }
     }
 
-    if (st_stack_pop(node_stack, &tmp) != ST_STACK_OK) {
-        ST_WARNING("Failed to st_stack_pop node.");
-        return -1;
-    }
-#ifdef _GRAPH_DEBUG_
-    ST_DEBUG("Node pop: %d", (int)(long)tmp);
-#endif
-    on_stack[start] = false;
-    if (recur_lk >= 0) {
-        post_order[*post_i] = -recur_lk;// store the recur_lk as negtive value
+    args->on_stack[start] = false;
+    if (recur_i > 0) {
+        post_order[args->post_i] = -(start + 1); // avoid start == 0
     } else {
-        post_order[*post_i] = start;
+        post_order[args->post_i] = start;
     }
-    *post_i += 1;
+    args->post_i += 1;
+
+    return 0;
+}
+
+/*
+ * in-place change the post order to forward order.
+ * len(recur_to) == num_node * num_node, store the dest nodes for all
+ * recur nodes. The recur node should be in front of its dest nodes.
+ */
+static int post2forward_order(int *post_order, int num_node, int *recur_to)
+{
+    int i, j, n, pos;
+    int recur_node, t;
+    int *to_nodes;
+
+    int prev_node_left, recur_node_left;
+
+    ST_CHECK_PARAM(post_order == NULL || recur_to == NULL, -1);
+
+    /* reverse to get forward order. */
+    for (n = 0; n < num_node / 2; n++) {
+        t = post_order[n];
+        post_order[n] = post_order[num_node - 1 - n];
+        post_order[num_node - 1 - n] = t;
+    }
+
+    prev_node_left = -1;
+    while (true) {
+        recur_node_left = 0;
+        /* find recur nodes and insert them to the right position. */
+        for (n = 0; n < num_node; n++) {
+            if (post_order[n] < 0) {
+                recur_node = -post_order[n] - 1;
+                to_nodes = recur_to + recur_node*num_node;
+                if (to_nodes[0] < 0) {
+                    ST_WARNING("No recur_to for recur_node[%d]", recur_node);
+                    return -1;
+                }
+
+                /* find the most front dest node. */
+                i = 0;
+                pos = num_node;
+                while (to_nodes[i] >= 0) {
+                    for (j = 0; j < num_node; j++) {
+                        if (-post_order[j] - 1 == to_nodes[i]) {
+                            // our dest node is a recur node and
+                            // it hasn't found its own position,
+                            // so we wait for next round
+                            recur_node_left++;
+                            goto SKIP_ONE;
+                        }
+
+                        if (post_order[j] == to_nodes[i]) {
+                            if (j < pos) {
+                                pos = j;
+                            }
+                            break;
+                        }
+                    }
+                    i++;
+                }
+                if (pos >= num_node) {
+                    ST_WARNING("dest node[%d] not found in forward order",
+                            to_nodes[i]);
+                    return -1;
+                }
+
+                // insert this recur node to front of pos
+                if (pos == n) {
+                    ST_WARNING("dest node should not equal to recur_node.");
+                    return -1;
+                } else if (pos > n) {
+                    for (i = n; i < pos - 1; i++) {
+                        post_order[i] = post_order[i + 1];
+                    }
+                    post_order[pos - 1] = recur_node;
+                } else {
+                    for (i = n; i >= pos + 1; i--) {
+                        post_order[i] = post_order[i - 1];
+                    }
+                    post_order[pos] = recur_node;
+                }
+            }
+SKIP_ONE:
+            continue;
+        }
+
+        if (recur_node_left <= 0) {
+            break;
+        }
+
+        if (prev_node_left == recur_node_left) {
+            ST_WARNING("Something went wrong, we can't find a proper "
+                       "forward order for the graph.");
+            return -1;
+        }
+
+        prev_node_left = recur_node_left;
+    }
 
     return 0;
 }
@@ -189,17 +339,15 @@ static int graph_dfs(graph_t *graph, int start,
  */
 int* graph_sort(graph_t *graph)
 {
-    st_stack_t *node_stack = NULL; /* node stack. */
-    bool *on_stack = NULL; /* node whether on current stack. */
-    bool *visited = NULL; /* node whether visited. */
-    bool *passed = NULL; /* link whether passed. */
-    int *post_order = NULL; /* sequence for post-order traverse. */
-    int post_i;
+    dfs_args_t args;
+    int *post_order = NULL;
 
-    int n, t;
+    int n;
     int l;
 
     ST_CHECK_PARAM(graph == NULL, NULL);
+
+    memset(&args, 0, sizeof(dfs_args_t));
 
     post_order = (int *)malloc(sizeof(int) * graph->num_node);
     if (post_order == NULL) {
@@ -224,64 +372,34 @@ int* graph_sort(graph_t *graph)
         goto ERR;
     }
 
-    node_stack = st_stack_create((st_stack_id_t)graph->num_node);
-    if (node_stack == NULL) {
-        ST_WARNING("Failed to st_stack_create node_stack.");
+    if (dfs_args_init(&args, graph) < 0) {
+        ST_WARNING("Failed to dfs_args_init.");
         goto ERR;
     }
-    (void)st_stack_clear(node_stack);
 
-    on_stack = (bool *)malloc(sizeof(bool) * graph->num_node);
-    if (on_stack == NULL) {
-        ST_WARNING("Failed to malloc on_stack");
-        goto ERR;
-    }
-    memset(on_stack, 0, sizeof(bool) * graph->num_node);
-
-    visited = (bool *)malloc(sizeof(bool) * graph->num_node);
-    if (visited == NULL) {
-        ST_WARNING("Failed to malloc visited");
-        goto ERR;
-    }
-    memset(visited, 0, sizeof(bool) * graph->num_node);
-
-    passed = (bool *)malloc(sizeof(bool) * graph->num_link);
-    if (passed == NULL) {
-        ST_WARNING("Failed to malloc passed");
-        goto ERR;
-    }
-    memset(passed, 0, sizeof(bool) * graph->num_link);
-
-    post_i = 0;
-    if (graph_dfs(graph, 0, node_stack, on_stack,
-                visited, passed, post_order, &post_i) < 0) {
+    if (graph_dfs(graph, 0, post_order, &args) < 0) {
         ST_WARNING("Failed to dfs.");
         goto ERR;
     }
 
     for (n = 0; n < graph->num_node; n++) {
-        if (!visited[n]) {
+        if (!args.visited[n]) {
             ST_WARNING("Node[%d] not visited.", n);
             goto ERR;
         }
     }
 
     for (l = 0; l < graph->num_link; l++) {
-        if (!passed[l]) {
+        if (!args.passed[l]) {
             ST_WARNING("link[%d] not visited.", l);
             goto ERR;
         }
     }
 
-    if (post_i != graph->num_node) {
+    if (args.post_i != graph->num_node) {
         ST_WARNING("post order not finished.");
         goto ERR;
     }
-
-    safe_st_stack_destroy(node_stack);
-    safe_free(on_stack);
-    safe_free(visited);
-    safe_free(passed);
 
 #ifdef _GRAPH_DEBUG_
     ST_DEBUG("Post order:");
@@ -289,11 +407,10 @@ int* graph_sort(graph_t *graph)
         ST_DEBUG("%d", post_order[n]);
     }
 #endif
-    /* reverse to get forward order. */
-    for (n = 0; n < graph->num_node / 2; n++) {
-        t = post_order[n];
-        post_order[n] = post_order[graph->num_node - 1 - n];
-        post_order[graph->num_node - 1 - n] = t;
+    if (post2forward_order(post_order, graph->num_node,
+                args.recur_to) < 0) {
+        ST_WARNING("Failed to post2forward_order.");
+        goto ERR;
     }
 #ifdef _GRAPH_DEBUG_
     ST_DEBUG("Forward order:");
@@ -302,13 +419,12 @@ int* graph_sort(graph_t *graph)
     }
 #endif
 
+    dfs_args_destroy(&args);
+
     return post_order;
 
 ERR:
-    safe_st_stack_destroy(node_stack);
-    safe_free(on_stack);
-    safe_free(visited);
-    safe_free(passed);
+    dfs_args_destroy(&args);
     safe_free(post_order);
 
     return NULL;
@@ -403,7 +519,7 @@ graph_t* graph_construct(layer_t **layers, int n_layer,
         ST_DEBUG("Layer[%d/%s]:%d", l,
                 layers[l]->name, out_glues[l].num_glue);
         for (g = 0; g < out_glues[l].num_glue; g++) {
-            ST_DEBUG("    -> "GLUE_ID_FMT"/%s",
+            ST_DEBUG("    -> %d/%s",
                     out_glues[l].glues[g],
                     glues[out_glues[l].glues[g]]->name);
         }
@@ -438,8 +554,7 @@ graph_t* graph_construct(layer_t **layers, int n_layer,
     ST_DEBUG("Graph: node: %d, link: %d",
             graph->num_node, graph->num_link);
     for (g = 0; g < graph->num_node; g++) {
-        ST_DEBUG("Node[%d/%s]: %d", g,
-                glues[graph->nodes[g].glue]->name,
+        ST_DEBUG("Node[%d/%s]: %d", g, glues[g]->name,
                 graph->nodes[g].num_link);
         for (lk = 0; lk < graph->nodes[g].num_link; lk++) {
             ST_DEBUG(" %d -> %d/%s",
