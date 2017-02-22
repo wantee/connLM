f97e754ede1ca1db6fef40c54fabe33737a34136
Wang Jian <wantee.wang@gmail.com>
2017-02-22 21:08:29 +0800

    change sed pattern to POSIX style

--- egs/utils/get_value.sh
+++ egs/utils/get_value.sh
@@ -9,8 +9,8 @@ sep="[:=]"
 pattern="$1\s*$sep\s*[-+0-9\.]*|$1\s*$sep\s*[Tt][Rr][Uu][Ee]|$1\s*$sep\s*[Ff][Aa][Ll][Ss][e]|$1\s*$sep\s*[-+]?inf|$1\s*$sep\s*[-+]?nan"
 if [ $# -ge 2 ]; then
     cat $2 | grep -E -o "$pattern" \
-      | tail -n 1 | awk -F"$sep" '{print $2}' | sed -e 's/^\s*//' -e 's/\s*$//g'
+      | tail -n 1 | awk -F"$sep" '{print $2}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//g'
 else
     grep -E -o "$pattern" \
-      | tail -n 1 | awk -F"$sep" '{print $2}' | sed -e 's/^\s*//' -e 's/\s*$//g'
+      | tail -n 1 | awk -F"$sep" '{print $2}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//g'
 fi
