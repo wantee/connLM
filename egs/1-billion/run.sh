a76a5b5c59dfe95b0ddd6f51e95ad9cebaad0937
Wang Jian <wantee.wang@gmail.com>
2017-02-23 20:44:10 +0800

    update configs for egs/1-billion

--- egs/1-billion/run.sh
+++ egs/1-billion/run.sh
@@ -13,19 +13,16 @@ vocab_file=./data/vocab
 conf_dir=./conf/
 exp_dir=./exp/
 
-#class_size=""
-class_size="1200"
-tr_thr=12
-eval_thr=12
+tr_thr=16
+eval_thr=16
 
 realtype="float"
 
 stepnames[1]="Prepare data"
-stepnames[2]="Learn Vocab"
-stepnames[3]="Train MaxEnt model"
-stepnames[4]="Train RNN model"
-stepnames[5]="Train RNN+MaxEnt model"
-stepnames[6]="Train RNN~MaxEnt merge model"
+stepnames+=("Learn Vocab")
+stepnames+=("Train MaxEnt model:maxent")
+stepnames+=("Train RNN model:rnn")
+stepnames+=("Train RNN+MaxEnt model:rnn+maxent")
 
 steps_len=${#stepnames[*]}
 
@@ -37,7 +34,7 @@ function print_help()
   st=1
   while [ $st -le $steps_len ]
   do
-  echo "   step$st:	${stepnames[$st]}"
+  echo "   step$st:	${stepnames[$st]%%:*}"
   ((st++))
   done
   echo ""
@@ -98,50 +95,21 @@ echo "Step $st: ${stepnames[$st]} ..."
 fi
 ((st++))
 
-if shu-in-range $st $steps; then
-echo
-echo "Step $st: ${stepnames[$st]} ..."
-../steps/run_standalone.sh --class-size "$class_size" \
-      --train-thr $tr_thr --eval-thr $eval_thr \
-    maxent $conf_dir $exp_dir $train_file $valid_file $test_file || exit 1;
-fi
-((st++))
-
-if shu-in-range $st $steps; then
-echo
-echo "Step $st: ${stepnames[$st]} ..."
-../steps/run_standalone.sh --class-size "$class_size" \
-      --train-thr $tr_thr --eval-thr $eval_thr \
-    rnn $conf_dir $exp_dir $train_file $valid_file $test_file || exit 1;
-fi
-((st++))
-
-if shu-in-range $st $steps; then
-echo
-echo "Step $st: ${stepnames[$st]} ..."
-../steps/run_standalone.sh --class-size "$class_size" \
-      --train-thr $tr_thr --eval-thr $eval_thr \
-    rnn+maxent $conf_dir $exp_dir $train_file $valid_file $test_file \
-  || exit 1;
-fi
-((st++))
-
-if shu-in-range $st $steps; then
-echo
-echo "Step $st: ${stepnames[$st]} ..."
-../steps/run_cascade.sh --class-size "$class_size" \
-      --train-thr $tr_thr --eval-thr $eval_thr \
-    maxent~rnn $conf_dir $exp_dir $train_file $valid_file $test_file \
-  || exit 1;
-fi
-((st++))
-
-if shu-in-range $st $steps; then
-echo
-echo "Step $st: ${stepnames[$st]} ..."
-../steps/run_cascade.sh --class-size "$class_size" \
-      --train-thr $tr_thr --eval-thr $eval_thr \
-   rnn~maxent $conf_dir $exp_dir $train_file $valid_file $test_file \
-  || exit 1;
-fi
-((st++))
+while [ $st -le $steps_len ]
+do
+  if shu-in-range $st $steps; then
+  echo
+  echo "Step $st: ${stepnames[$st]%%:*} ..."
+  model=${stepnames[$st]#*:}
+  if [[ "$model" == *"~"* ]]; then
+    ../steps/run_cascade.sh --train-thr $tr_thr --eval-thr $eval_thr \
+        ${model} $conf_dir $exp_dir \
+        $train_file $valid_file $test_file || exit 1;
+  else
+    ../steps/run_standalone.sh --train-thr $tr_thr --eval-thr $eval_thr \
+        ${model} $conf_dir $exp_dir \
+        $train_file $valid_file $test_file || exit 1;
+  fi
+  fi
+  ((st++))
+done
