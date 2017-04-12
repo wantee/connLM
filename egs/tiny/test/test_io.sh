5f82a79852076666605d4ed0b4c4eba6b18df191
Wang Jian <wantee.wang@gmail.com>
2017-04-12 22:17:23 +0800

    fix test_io.sh against new --format option

--- egs/tiny/test/test_io.sh
+++ egs/tiny/test/test_io.sh
@@ -18,15 +18,20 @@ train_file=${3:-./data/train}
 
 shu-run connlm-info $exp_dir/init.clm || exit 1
 
-shu-run connlm-copy --binary=true $exp_dir/init.clm $exp_dir/bin.clm || exit 1
+shu-run connlm-copy --format=bin $exp_dir/init.clm $exp_dir/bin.clm || exit 1
 
-shu-run connlm-copy --binary=false $exp_dir/bin.clm $exp_dir/txt.clm || exit 1
+shu-run connlm-copy --format=txt $exp_dir/bin.clm $exp_dir/txt.clm || exit 1
 
-shu-run connlm-copy --binary=false $exp_dir/txt.clm $exp_dir/txt1.clm || exit 1
+shu-run connlm-copy --format=sq $exp_dir/txt.clm $exp_dir/sq.clm || exit 1
 
-shu-run connlm-copy --binary=true $exp_dir/txt.clm $exp_dir/bin1.clm || exit 1
+shu-run connlm-copy --format=zc $exp_dir/sq.clm $exp_dir/zc.clm || exit 1
 
-diff $exp_dir/txt.clm $exp_dir/txt1.clm || exit
+shu-run connlm-copy --format="'sq|zc'" $exp_dir/zc.clm $exp_dir/sqzc.clm || exit 1
+
+shu-run connlm-copy --format=bin $exp_dir/sqzc.clm $exp_dir/bin1.clm || exit 1
+
+shu-run connlm-copy --format=txt $exp_dir/txt.clm $exp_dir/txt1.clm || exit 1
+diff $exp_dir/txt.clm $exp_dir/txt1.clm || exit 1
 
 echo "All Passed."
 exit 0
