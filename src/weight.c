be9a4fa45cabb5670460afc4be401541f34304df
Jian Wang <wantee.wang@gmail.com>
2017-10-15 23:29:38 +0800

    remove the restriction on constant param init value

--- src/weight.c
+++ src/weight.c
@@ -197,10 +197,12 @@ int wt_parse_topo(weight_t *wt, char *line, size_t line_len)
         if (isnan(wt->init_param)) {
             wt->init_param = 0;
         }
+#if 0
         if (wt->init_param != 0.0) {
             ST_WARNING("WT_INIT_CONST should be 0.0.");
             wt->init_param = 0;
         }
+#endif
     } else if (wt->init_type == WT_INIT_UNIFORM) {
         if (isnan(wt->init_param)) {
             wt->init_param = 0.1;
@@ -557,7 +559,8 @@ int wt_init(weight_t *wt, size_t row, size_t col)
 
     switch(wt->init_type) {
         case WT_INIT_CONST:
-            mat_set(&wt->w, 0.0);
+            mat_set(&wt->w, wt->init_param);
+            break;
         case WT_INIT_UNIFORM:
             for (i = 0; i < wt->w.num_rows; i++) {
                 for (j = 0; j < wt->w.num_cols; j++) {
@@ -572,6 +575,7 @@ int wt_init(weight_t *wt, size_t row, size_t col)
                     MAT_VAL(&wt->w, i, j) = st_normrand(0, wt->init_param);
                 }
             }
+            break;
         case WT_INIT_TRUNC_NORM:
             for (i = 0; i < wt->w.num_rows; i++) {
                 for (j = 0; j < wt->w.num_cols; j++) {
